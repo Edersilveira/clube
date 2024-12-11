3243e7902db09e49193a04d4f86fcbc4699b37e0
Eder <edersylveira@gmail.com>
2024-12-11 11:07:29 -0300

    primeira esboco site clube

---
+++ scripts.js
@@ -0,0 +1,3 @@
+// Scripts básicos para interações futuras
+// Aqui você pode adicionar código para validação de formulários, integração com APIs, etc.
+console.log("Site do Clube Social carregado com sucesso!");
